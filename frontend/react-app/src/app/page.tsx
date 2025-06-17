58917e5679c0379ad17ef9258296198d8e4832f0
Dhyey <dn6patel@uwaterloo.ca>
2025-06-17 01:39:33 -0400

    Modify Home component

--- frontend/react-app/src/app/page.tsx
+++ frontend/react-app/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Greeting from "../../components/Greeting/Greeting";
+import Styles from "./page.module.scss";
+
 export default function Home() {
-  return;
+  return (
+    <div className={Styles.mainContainer}>
+      <Greeting />
+    </div>
+  );
 }
