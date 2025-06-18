525e35d73e0466bbcc96d53b7b6ec2212b5f6538
Dhyey <dn6patel@uwaterloo.ca>
2025-06-18 03:46:04 -0400

    Use Chatbox component

--- frontend/react-app/src/app/page.tsx
+++ frontend/react-app/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Greeting from "../../components/Greeting/Greeting";
+import Chatbox from "../../components/Chatbox/Chatbox";
 import Styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={Styles.mainContainer}>
       <Greeting />
+      <Chatbox />
     </div>
   );
 }
