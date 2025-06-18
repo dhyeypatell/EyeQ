8b8f302d76a80a4ed403ad0a56792651c3ece422
Dhyey <dn6patel@uwaterloo.ca>
2025-06-18 04:53:36 -0400

    Create state in Chatbox

--- frontend/react-app/components/Chatbox/Chatbox.tsx
+++ frontend/react-app/components/Chatbox/Chatbox.tsx
@@ -1,12 +1,22 @@
+"use client";
 import data from "./Chatbox.json";
 import Styles from "./Chatbox.module.scss";
+import { useState } from "react";
 
 export default function Chatbox() {
+  const [userMessage, setUserMessage] = useState("");
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setUserMessage(e.target.value);
+  }
+
   return (
     <textarea
       name="prompt"
       placeholder={data.Chatbox.placeholder}
       className={Styles.chatInput}
+      value={userMessage}
+      onChange={handleChange}
     />
   );
 }
