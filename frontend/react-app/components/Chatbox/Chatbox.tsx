2868fa049cbfe2fffd774594cfc5bd312c0a0ab9
Dhyey <dn6patel@uwaterloo.ca>
2025-06-18 09:03:47 -0400

    Add up-arrow icon to Chatbox

--- frontend/react-app/components/Chatbox/Chatbox.tsx
+++ frontend/react-app/components/Chatbox/Chatbox.tsx
@@ -1,6 +1,7 @@
 "use client";
 import data from "./Chatbox.json";
 import Styles from "./Chatbox.module.scss";
+import UpArrowIcon from "../../src/assets/icons/up-arrow-circle.svg";
 import { useState } from "react";
 
 export default function Chatbox() {
@@ -11,12 +12,17 @@ export default function Chatbox() {
   }
 
   return (
-    <textarea
-      name="prompt"
-      placeholder={data.Chatbox.placeholder}
-      className={Styles.chatInput}
-      value={userMessage}
-      onChange={handleChange}
-    />
+    <div className={Styles.container}>
+      <textarea
+        name="prompt"
+        placeholder={data.Chatbox.placeholder}
+        className={Styles.chatInput}
+        value={userMessage}
+        onChange={handleChange}
+      />
+      <div className={Styles.iconContainer}>
+        <UpArrowIcon className={Styles.icon} />
+      </div>
+    </div>
   );
 }
