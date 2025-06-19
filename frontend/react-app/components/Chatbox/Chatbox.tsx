452107d2aa693dba4ce7cbb2a3a1bb84bab1e467
Dhyey <dn6patel@uwaterloo.ca>
2025-06-19 00:31:18 -0400

    Modify Chatbox

--- frontend/react-app/components/Chatbox/Chatbox.tsx
+++ frontend/react-app/components/Chatbox/Chatbox.tsx
@@ -2,26 +2,38 @@
 import data from "./Chatbox.json";
 import Styles from "./Chatbox.module.scss";
 import UpArrowIcon from "../../src/assets/icons/up-arrow-circle.svg";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Chatbox() {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const iconRef = useRef<HTMLDivElement>(null);
   const [userMessage, setUserMessage] = useState("");
+
+  function handleContainerClick(e: React.MouseEvent<HTMLDivElement>) {
+    if (iconRef.current?.contains(e.target as Node)) {
+      textareaRef.current?.blur();
+      return;
+    }
+
+    textareaRef.current?.focus();
+  }
 
   function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     setUserMessage(e.target.value);
   }
 
   return (
-    <div className={Styles.container}>
+    <div className={Styles.container} onClick={handleContainerClick}>
       <textarea
         name="prompt"
         placeholder={data.Chatbox.placeholder}
+        ref={textareaRef}
         className={Styles.chatInput}
         value={userMessage}
         onChange={handleChange}
       />
       <div className={Styles.iconContainer}>
-        <UpArrowIcon className={Styles.icon} />
+        <UpArrowIcon ref={iconRef} className={Styles.icon} />
       </div>
     </div>
   );
