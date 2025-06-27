0380bdd1244abd6128c1f6c0901e5cde561b8c0e
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2025-06-26 22:22:26 -0400

    Setup and use MongoDB as the app's database (#21)
    - Setup MongoDB database online
    - Added .env file to store env variables
    - Modified main.py to insert and find documents in the app's database
    - Added a payload field in the post request
    - Updated how the post and get requests are handled in the backend API
    - Updated readme file with live backend server link

--- backend/api/README.md
+++ backend/api/README.md
@@ -1,6 +1,7 @@
 # Inkling backend
 
 - The backend is built using Python
+- The backend server is live at [https://inkling-backend.onrender.com](https://inkling-backend.onrender.com)
 
 ## The backend is currently being developed
 
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ENV = os.getenv("ENV", "dev")
+MONGODB_URI = os.getenv("MONGODB_URI")
+DB_NAME = f"inkling-{ENV}"
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+collection = db["messages"]
 
 app = FastAPI()
 
@@ -11,8 +24,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-user_messages = []
 
 
 def get_timestamp():
@@ -25,13 +36,32 @@ def get_timestamp():
 async def post_user_message(request: Request):
     data = await request.json()
     payload = {
+        "type": data.get("type"),
         "userMessage": data.get("userMessage"),
         "timestamp": data.get("timestamp", get_timestamp()),
     }
-    user_messages.append(payload)
-    return payload
+
+    try:
+        collection.insert_one(payload)
+    except Exception:
+        raise HTTPException(
+            status_code=500, detail="Failed to insert document into the database"
+        )
 
 
 @app.get("/api/user-messages")
 async def get_user_messages():
-    return {"userMessages": user_messages}
+    try:
+        cursor = collection.find({"type": "user"})
+    except Exception:
+        raise HTTPException(
+            status_code=500, detail="Failed to retrieve user messages from the database"
+        )
+
+    userMessages = []
+
+    for doc in cursor:
+        doc["_id"] = str(doc["_id"])
+        userMessages.append(doc)
+
+    return {"messages": userMessages}
