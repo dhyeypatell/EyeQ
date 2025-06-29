96ef37826ce304a79479ce0143f0824c38659200
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2025-06-29 17:49:58 -0400

    Update the main page to handle user-submitted data (#23)
    - Built and used UserMessageBlock and AIMessageBlock in the main page
    - Modified Chatbox
    - Styled the main page layout
    - Made global function validateSender
    - Added a new color
    - Defined global types

--- frontend/react-app/src/app/page.tsx
+++ frontend/react-app/src/app/page.tsx
@@ -1,12 +1,49 @@
+"use client";
+import { useState } from "react";
+import clsx from "clsx";
+import type { ChatMessage } from "../types/chatMessage";
 import Greeting from "../../components/Greeting/Greeting";
 import Chatbox from "../../components/Chatbox/Chatbox";
+import UserMessageBlock from "../../components/UserMessageBlock/UserMessageBlock";
+import AIMessageBlock from "../../components/AIMessageBlock/AIMessageBlock";
 import Styles from "./page.module.scss";
 
 export default function Home() {
+  const [userMessage, setUserMessage] = useState<string>("");
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+
+  function renderMessageBlock(chatMessage: ChatMessage, index: number) {
+    if (chatMessage.sender === "user") {
+      return <UserMessageBlock key={index} message={chatMessage.message} />;
+    } else if (chatMessage.sender === "ai") {
+      return <AIMessageBlock key={index} message={chatMessage.message} />;
+    }
+
+    return null;
+  }
+
   return (
-    <div className={Styles.mainContainer}>
-      <Greeting />
-      <Chatbox />
+    <div
+      className={clsx(
+        Styles.mainContainer,
+        chatMessages.length && Styles.chatLayout
+      )}
+    >
+      {!chatMessages.length ? (
+        <Greeting />
+      ) : (
+        <div className={Styles.chatMessagesContainer}>
+          {chatMessages.map((chatMessage, index) =>
+            renderMessageBlock(chatMessage, index)
+          )}
+        </div>
+      )}
+      <Chatbox
+        userMessage={userMessage}
+        setUserMessage={setUserMessage}
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
     </div>
   );
 }
